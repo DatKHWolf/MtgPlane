eb85b5835d76f6580fc5c2ae94e42ebd073b32d1
datKHWolf <dkhwolf@gmail.com>
2021-10-03 04:03:54 +0200

    Card Klasse

--- main.py
+++ main.py
@@ -2,36 +2,15 @@ from flask import Flask, render_template, url_for
 import requests
 import random
 from CardList import cardlist
+from Card import Card
 
 
 app = Flask(__name__)
-my_list = cardlist([])
-
-
-class Card:
-    def __init__(self, name, oracle_text, chaos_text, image, rules):
-        self.name = name
-        self.oracle_text = oracle_text
-        self.chaos_text = chaos_text
-        self.image = image
-        self.rules = rules
-
-    # Testen
-    def print_name(self):
-        print(self.name)
-
-    def print_chaos_text(self):
-        print(self.chaos_text)
-
-    def print_oracle_text(self):
-        print(self.oracle_text)
-
-    def print_image(self):
-        print(self.image)
 
 
 @app.route("/")
 def get_planes():
+    my_list = cardlist([])
     url = 'https://api.scryfall.com/cards/search?q=t%3Aplane'
     req = requests.get(url)
     card_list = req.json()['data']
